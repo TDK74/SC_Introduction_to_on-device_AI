2e9f567c11dad6eb26d1f81814fa7e2d46d9f1d9
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2026-08-10 15:14:04 +0300

    Refactor L3_Student.py by removing redundant code.

--- L3_Student.py
+++ L3_Student.py
@@ -28,8 +28,7 @@ for device in qai_hub.get_devices():
     print(device.name)
 
 ## ------------------------------------------------------ ##
-devices = [
-            "Samsung Galaxy S22 Ultra 5G",
+devices = ["Samsung Galaxy S22 Ultra 5G",
             "Samsung Galaxy S22 5G",
             "Samsung Galaxy S22+ 5G",
             "Samsung Galaxy Tab S8",
@@ -41,8 +40,7 @@ devices = [
             "Samsung Galaxy S23 Ultra",
             "Samsung Galaxy S24",
             "Samsung Galaxy S24 Ultra",
-            "Samsung Galaxy S24+",
-        ]
+            "Samsung Galaxy S24+", ]
 
 selected_device = random.choice(devices)
 print(selected_device)
@@ -50,11 +48,9 @@ print(selected_device)
 ## ------------------------------------------------------ ##
 device = qai_hub.Device(selected_device)
 
-compile_job = qai_hub.submit_compile_job(
-            model = traced_model,
-            input_specs = {"image": input_shape},
-            device = device,
-            )
+compile_job = qai_hub.submit_compile_job(model = traced_model,
+                                        input_specs = {"image" : input_shape},
+                                        device = device, )
 
 ## ------------------------------------------------------ ##
 target_model = compile_job.get_target_model()
@@ -64,20 +60,15 @@ compile_options = "--target_runtime tflite"
 compile_options = "--target_runtime onnx"
 compile_options = "--target_runtime qnn_lib_aarch64_android"
 
-compile_job_expt = qai_hub.submit_compile_job(
-                model = traced_model,
-                input_specs = {"image": input_shape},
-                device = device,
-                options = compile_options,
-                )
+compile_job_expt = qai_hub.submit_compile_job(model = traced_model,
+                                            input_specs = {"image" : input_shape},
+                                            device = device,
+                                            options = compile_options, )
 
 ## ------------------------------------------------------ ##
 device = qai_hub.Device(selected_device)
 
-profile_job = qai_hub.submit_profile_job(
-                model = target_model,
-                device = device,
-                )
+profile_job = qai_hub.submit_profile_job(model = target_model, device = device, )
 
 profile_data = profile_job.download_profile()
 print_profile_metrics_from_job(profile_job, profile_data)
@@ -87,11 +78,9 @@ profile_options = "--compute_unit cpu"
 profile_options = "--compute_unit gpu"
 profile_options = "--compute_unit npu"
 
-profile_job_expt = qai_hub.submit_profile_job(
-                    model = target_model,
-                    device = device,
-                    options = profile_options,
-                    )
+profile_job_expt = qai_hub.submit_profile_job(model = target_model,
+                                            device = device,
+                                            options = profile_options, )
 
 ## ------------------------------------------------------ ##
 sample_inputs = ffnet_40s.sample_inputs()
@@ -103,11 +92,9 @@ torch_outputs = ffnet_40s(torch_inputs)
 print(torch_outputs)
 
 ## ------------------------------------------------------ ##
-inference_job = qai_hub.submit_inference_job(
-                model = target_model,
-                inputs = sample_inputs,
-                device = device,
-                )
+inference_job = qai_hub.submit_inference_job(model = target_model,
+                                            inputs = sample_inputs,
+                                            device = device, )
 
 ## ------------------------------------------------------ ##
 ondevice_outputs = inference_job.download_output_data()
