7142476c06fa8c13017a87ea5413183ae80320da
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2026-08-12 16:48:39 +0300

    Python file L4_Student.py added.

---
+++ L4_Student.py
@@ -0,0 +1,127 @@
+import random
+import numpy as np
+import qai_hub
+import qai_hub_models
+import torch
+import torch.nn.functional as F
+
+from aimet_torch.batch_norm_fold import fold_all_batch_norms
+from aimet_torch.model_preparer import prepare_model
+from aimet_torch.quantsim import QuantizationSimModel
+from datasets import load_dataset
+from PIL import Image
+from qai_hub_models.models._shared.ffnet_quantized.model import FFNET_AIMET_CONFIG
+from qai_hub_models.models.ffnet_40s.model import FFNet40S
+from torchvision import transforms
+from utils import get_ai_hub_api_token
+
+
+## ------------------------------------------------------ ##
+input_shape = (1, 3, 1024, 2048)
+
+dataset = load_dataset("UrbanSyn/UrbanSyn",
+                        split = "train",
+                        data_files = "rgb/*_00*.png")
+dataset = dataset.train_test_split(1)
+calibration_dataset = dataset["train"]
+test_dataset = dataset["test"]
+
+## ------------------------------------------------------ ##
+calibration_dataset["image"][0]
+
+## ------------------------------------------------------ ##
+preprocess = transforms.ToTensor()
+
+test_sample_pil = test_dataset[0]["image"]
+test_sample = preprocess(test_sample_pil).unsqueeze(0)
+print(test_sample)
+
+## ------------------------------------------------------ ##
+def postprocess(output_tensor, input_image_pil):
+    output_tensor_upsampled = F.interpolate(
+                                            output_tensor,
+                                            input_shape[2 : ],
+                                            mode = "bilinear",
+                                            )
+
+    output_predictions = (output_tensor_upsampled[0].
+                        argmax(0).byte().detach().
+                        numpy().astype(np.uint8))
+
+    color_mask = Image.fromarray(output_predictions).convert("P")
+
+    palette = [
+                128, 64, 128, 244, 35, 232, 70, 70, 70, 102, 102, 156,
+                190, 153, 153, 153, 153, 153, 250, 170, 30, 220, 220, 0,
+                107, 142, 35, 152, 251, 152, 70, 130, 180, 220, 20, 60,
+                255, 0, 0, 0, 0, 142, 0, 0, 70, 0, 60, 100, 0, 80, 100,
+                0, 0, 230, 119, 11, 32
+                ]
+    palette = palette + (256 * 3 - len(palette)) * [0]
+    color_mask.putpalette(palette)
+    out = Image.blend(input_image_pil, color_mask.convert("RGB"), 0.5)
+
+    return out
+
+## ------------------------------------------------------ ##
+model = FFNet40S.from_pretrained().model.eval()
+
+test_output_fp32 = model(test_sample)
+print(test_output_fp32)
+
+postprocess(test_output_fp32, test_sample_pil)
+
+## ------------------------------------------------------ ##
+fold_all_batch_norms(model, [input_shape])
+model = prepare_model(model)
+
+quant_sim = QuantizationSimModel(
+                                model,
+                                quant_scheme = "tf_enhanced",
+                                default_param_bw = 8,
+                                default_output_bw = 8,
+                                config_file = FFNET_AIMET_CONFIG,
+                                dummy_input = torch.rand(input_shape),
+                                )
+
+## ------------------------------------------------------ ##
+size = 5
+
+def pass_calibration_data(sim_model: torch.nn.Module, args):
+    (dataset,) = args
+
+    with torch.no_grad():
+        for sample in dataset.select(range(size)):
+            pil_image = sample["image"]
+            input_batch = preprocess(pil_image).unsqueeze(0)
+
+            sim_model(input_batch)
+
+quant_sim.compute_encodings(pass_calibration_data, [calibration_dataset])
+
+## ------------------------------------------------------ ##
+test_output_int8 = quant_sim.model(test_sample)
+postprocess(test_output_int8, test_sample_pil)
+
+## ------------------------------------------------------ ##
+ai_hub_api_token = get_ai_hub_api_token()
+
+## ------------------------------------------------------ ##
+devices = [
+            "Samsung Galaxy S22 Ultra 5G",
+            "Samsung Galaxy S22 5G",
+            "Samsung Galaxy S22+ 5G",
+            "Samsung Galaxy Tab S8",
+            "Xiaomi 12",
+            "Xiaomi 12 Pro",
+            "Samsung Galaxy S22 5G",
+            "Samsung Galaxy S23",
+            "Samsung Galaxy S23+",
+            "Samsung Galaxy S23 Ultra",
+            "Samsung Galaxy S24",
+            "Samsung Galaxy S24 Ultra",
+            "Samsung Galaxy S24+",
+            ]
+
+selected_device = random.choice(devices)
+print(selected_device)
